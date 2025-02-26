b30bf51115b1fb048663c728f5f86a631d3bebfc
mairoo <jonghwa@pincoin.co.kr>
2025-02-26 23:54:31 +0900

    redis config, security config

---
+++ src/main/kotlin/kr/co/pincoin/api/global/config/RedisConfig.kt
@@ -0,0 +1,19 @@
+package kr.co.pincoin.api.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        return RedisTemplate<String, String>().apply {
+            connectionFactory = redisConnectionFactory
+            keySerializer = StringRedisSerializer()
+            valueSerializer = StringRedisSerializer()
+        }
+    }
+}

1306b6c9dc676d130d1ff58a2f04b6a606f58f2f
mairoo <jonghwa@pincoin.co.kr>
2025-02-26 23:55:32 +0900

    keys

---
+++ src/main/kotlin/kr/co/pincoin/api/global/constant/CookieKey.kt
@@ -0,0 +1,7 @@
+package kr.co.pincoin.api.global.constant
+
+object CookieKey {
+    const val REFRESH_TOKEN_NAME = "refresh_token"
+    const val PATH = "/auth"
+    const val SAME_SITE = "Strict"
+}
---
+++ src/main/kotlin/kr/co/pincoin/api/global/constant/RedisKey.kt
@@ -0,0 +1,6 @@
+package kr.co.pincoin.api.global.constant
+
+object RedisKey {
+    const val EMAIL = "email"
+    const val IP_ADDRESS = "ipAddress"
+}

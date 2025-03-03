f9da2f3789e8c5d672191d1277deb5947ee69c6d
mairoo <jonghwa@pincoin.co.kr>
2025-03-03 19:20:06 +0900

    not found exception bugfix

--- src/main/kotlin/kr/co/pincoin/api/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/kr/co/pincoin/api/global/exception/GlobalExceptionHandler.kt
@@ -18,7 +18,7 @@ import org.springframework.web.bind.MissingRequestCookieException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.multipart.MaxUploadSizeExceededException
-import org.springframework.web.reactive.resource.NoResourceFoundException
+import org.springframework.web.servlet.resource.NoResourceFoundException
 import java.sql.SQLIntegrityConstraintViolationException
 
 @RestControllerAdvice

ca415faa641e5c439cebf4686a1318ff5472a671
mairoo <jonghwa@pincoin.co.kr>
2025-07-11 22:22:46 +0900

    profile params

--- src/main/kotlin/kr/pincoin/api/infra/user/entity/ProfileEntity.kt
+++ src/main/kotlin/kr/pincoin/api/infra/user/entity/ProfileEntity.kt
@@ -17,56 +17,23 @@ class ProfileEntity private constructor(
     @Embedded
     val dateTimeFields: DateTimeFields = DateTimeFields(),
 
-    @Column(name = "phone")
-    val phone: String? = null,
+    @Column(name = "user_id")
+    val userId: Int,
 
     @Column(name = "address")
     val address: String,
 
+    @Column(name = "phone")
+    val phone: String? = null,
+
     @Column(name = "phone_verified")
     val phoneVerified: Boolean,
-
-    @Column(name = "document_verified")
-    val documentVerified: Boolean,
-
-    @Column(name = "photo_id")
-    val photoId: String,
-
-    @Column(name = "card")
-    val card: String,
-
-    @Column(name = "total_order_count")
-    val totalOrderCount: Int = 0,
-
-    @Column(name = "last_purchased")
-    val lastPurchased: LocalDateTime?,
-
-    @Column(name = "max_price")
-    val maxPrice: BigDecimal,
-
-    @Column(name = "average_price")
-    val averagePrice: BigDecimal,
-
-    @Column(name = "user_id")
-    val userId: Int,
-
-    @Column(name = "memo")
-    val memo: String,
 
     @Column(name = "phone_verified_status")
     val phoneVerifiedStatus: Int,
 
     @Column(name = "date_of_birth")
     val dateOfBirth: LocalDate?,
-
-    @Column(name = "first_purchased")
-    val firstPurchased: LocalDateTime?,
-
-    @Column(name = "total_list_price")
-    val totalListPrice: BigDecimal,
-
-    @Column(name = "total_selling_price")
-    val totalSellingPrice: BigDecimal,
 
     @Column(name = "domestic")
     val domestic: Int,
@@ -77,11 +44,44 @@ class ProfileEntity private constructor(
     @Column(name = "telecom")
     val telecom: String,
 
+    @Column(name = "photo_id")
+    val photoId: String,
+
+    @Column(name = "card")
+    val card: String,
+
+    @Column(name = "document_verified")
+    val documentVerified: Boolean,
+
+    @Column(name = "total_order_count")
+    val totalOrderCount: Int = 0,
+
+    @Column(name = "first_purchased")
+    val firstPurchased: LocalDateTime?,
+
+    @Column(name = "last_purchased")
+    val lastPurchased: LocalDateTime?,
+
+    @Column(name = "max_price")
+    val maxPrice: BigDecimal,
+
+    @Column(name = "average_price")
+    val averagePrice: BigDecimal,
+
+    @Column(name = "total_list_price")
+    val totalListPrice: BigDecimal,
+
+    @Column(name = "total_selling_price")
+    val totalSellingPrice: BigDecimal,
+
     @Column(name = "not_purchased_months")
     val notPurchasedMonths: Boolean,
 
     @Column(name = "repurchased")
     val repurchased: LocalDateTime?,
+
+    @Column(name = "memo")
+    val memo: String,
 
     @Column(name = "mileage")
     val mileage: BigDecimal,
@@ -92,54 +92,54 @@ class ProfileEntity private constructor(
     companion object {
         fun of(
             id: Long? = null,
-            phone: String? = null,
-            address: String,
-            phoneVerified: Boolean = false,
-            documentVerified: Boolean = false,
-            photoId: String,
-            card: String,
-            totalOrderCount: Int = 0,
-            lastPurchased: LocalDateTime? = null,
-            maxPrice: BigDecimal = BigDecimal.ZERO,
-            averagePrice: BigDecimal = BigDecimal.ZERO,
             userId: Int,
-            memo: String,
+            address: String,
+            phone: String? = null,
+            phoneVerified: Boolean = false,
             phoneVerifiedStatus: Int = 0,
             dateOfBirth: LocalDate? = null,
-            firstPurchased: LocalDateTime? = null,
-            totalListPrice: BigDecimal = BigDecimal.ZERO,
-            totalSellingPrice: BigDecimal = BigDecimal.ZERO,
             domestic: Int = 0,
             gender: Int = 0,
             telecom: String,
+            photoId: String,
+            card: String,
+            documentVerified: Boolean = false,
+            totalOrderCount: Int = 0,
+            firstPurchased: LocalDateTime? = null,
+            lastPurchased: LocalDateTime? = null,
+            maxPrice: BigDecimal = BigDecimal.ZERO,
+            averagePrice: BigDecimal = BigDecimal.ZERO,
+            totalListPrice: BigDecimal = BigDecimal.ZERO,
+            totalSellingPrice: BigDecimal = BigDecimal.ZERO,
             notPurchasedMonths: Boolean = false,
             repurchased: LocalDateTime? = null,
+            memo: String,
             mileage: BigDecimal = BigDecimal.ZERO,
             allowOrder: Boolean = false,
         ) = ProfileEntity(
             id = id,
-            phone = phone,
-            address = address,
-            phoneVerified = phoneVerified,
-            documentVerified = documentVerified,
-            photoId = photoId,
-            card = card,
-            totalOrderCount = totalOrderCount,
-            lastPurchased = lastPurchased,
-            maxPrice = maxPrice,
-            averagePrice = averagePrice,
             userId = userId,
-            memo = memo,
+            address = address,
+            phone = phone,
+            phoneVerified = phoneVerified,
             phoneVerifiedStatus = phoneVerifiedStatus,
             dateOfBirth = dateOfBirth,
-            firstPurchased = firstPurchased,
-            totalListPrice = totalListPrice,
-            totalSellingPrice = totalSellingPrice,
             domestic = domestic,
             gender = gender,
             telecom = telecom,
+            photoId = photoId,
+            card = card,
+            documentVerified = documentVerified,
+            totalOrderCount = totalOrderCount,
+            firstPurchased = firstPurchased,
+            lastPurchased = lastPurchased,
+            maxPrice = maxPrice,
+            averagePrice = averagePrice,
+            totalListPrice = totalListPrice,
+            totalSellingPrice = totalSellingPrice,
             notPurchasedMonths = notPurchasedMonths,
             repurchased = repurchased,
+            memo = memo,
             mileage = mileage,
             allowOrder = allowOrder,
         )

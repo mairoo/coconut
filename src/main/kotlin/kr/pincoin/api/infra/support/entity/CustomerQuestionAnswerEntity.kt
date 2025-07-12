f0f4ee3237c9d9e09621bb63dace059fdfd8ae7b
mairoo <jonghwa@pincoin.co.kr>
2025-07-12 16:33:34 +0900

    order, support entities

--- src/main/kotlin/kr/pincoin/api/infra/support/entity/CustomerQuestionAnswerEntity.kt
+++ src/main/kotlin/kr/pincoin/api/infra/support/entity/CustomerQuestionAnswerEntity.kt
@@ -1,4 +1,34 @@
 package kr.pincoin.api.infra.support.entity
 
-class CustomerQuestionAnswerEntity {
+import jakarta.persistence.*
+import kr.pincoin.api.infra.common.jpa.DateTimeFields
+
+@Entity
+@Table(name = "shop_questionanswer")
+class CustomerQuestionAnswerEntity private constructor(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long?,
+
+    @Embedded
+    val dateTimeFields: DateTimeFields = DateTimeFields(),
+
+    @Column(name = "content")
+    val content: String,
+
+    @Column(name = "question_id")
+    val questionId: Long,
+) {
+    companion object {
+        fun of(
+            id: Long? = null,
+            content: String,
+            questionId: Long,
+        ) = CustomerQuestionAnswerEntity(
+            id = id,
+            content = content,
+            questionId = questionId,
+        )
+    }
 }

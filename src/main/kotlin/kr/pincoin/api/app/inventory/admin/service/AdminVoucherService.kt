87e1bed58e20ed94bdce89ac90207431f237cfd6
mairoo <jonghwa@pincoin.co.kr>
2025-07-16 23:39:32 +0900

    service auto build

--- src/main/kotlin/kr/pincoin/api/app/inventory/admin/service/AdminVoucherService.kt
+++ src/main/kotlin/kr/pincoin/api/app/inventory/admin/service/AdminVoucherService.kt
@@ -1,4 +1,9 @@
 package kr.pincoin.api.app.inventory.admin.service
 
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
 class AdminVoucherService {
 }

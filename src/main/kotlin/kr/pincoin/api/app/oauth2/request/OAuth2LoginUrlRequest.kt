224f07d8af3fd547167a039de82c491fac8760c0
jonghwa <jonghwa@pincoin.co.kr>
2025-08-02 19:45:00 +0900

    redirectUri bugfix

--- src/main/kotlin/kr/pincoin/api/app/oauth2/request/OAuth2LoginUrlRequest.kt
+++ src/main/kotlin/kr/pincoin/api/app/oauth2/request/OAuth2LoginUrlRequest.kt
@@ -3,6 +3,6 @@ package kr.pincoin.api.app.oauth2.request
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class OAuth2LoginUrlRequest(
-    @JsonProperty("redirect_uri")
+    @JsonProperty("redirectUri")
     val redirectUri: String,
 )
